2a0ce8acdf0d5935fa515ded1eb9deec4a87c3b2
Enes Tasdemir <enestasdemir5443@gmail.com>
2024-05-24 11:25:38 +0200

    Initial Commit v1.0

---
+++ trailsapp/src/configure.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line
+import React, { useState, useEffect } from 'react';
+
+function Configure() {
+  
+
+  return (
+    <div>
+        <header>
+
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+            <div>
+                <button class="border rounded-lg px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 mr-3">
+                    Bestätigen
+                </button>
+                <button class="border rounded-lg px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 mr-3">
+                    Abbrechen
+                </button>
+            </div>
+        </footer>
+    </div>
+  );
+}
+
+export default Configure;
